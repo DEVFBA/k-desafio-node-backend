7bf43308720be15eedc6a21d8af0d4fd2a2d9b94
DavidVazquez001 <dvazquez.glife@gmail.com>
2024-03-07 17:52:14 -0600

    David branch, primero commit

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,70 +1,24 @@
 //Imports
-
-
-
-
-
-
+const Messages = require("../models/messages");
 
 //Get posts Angel
 
-
-
-
-
-
-
-
 // Get Post por id
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 //Post Posts Angie
 
-
-
-
-
-
-
-
-
-
-
 //Put Posts David
-
-
-
-
-
-
-
-
-
-
-
-
+router.put("/:messageid", auth.validUser, async (req, res) => {
+  try {
+    const { messageid } = req.params;
+    const message = req.body;
+    const upMsg = await Messages.findByIdAndUpdate(messageid, message, {
+      returnOriginal: false,
+    });
+    res.status(200).send({ message: "Message sent", data: upMsg });
+  } catch (error) {
+    res.status(400).send({ message: error });
+  }
+});
 
 // Delete Posts Sadiel
-
-
-
-
-
-
-
-
-
-
-
