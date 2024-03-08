f1f8d2d8872d7205d0383942a04dc4cace288a8a
DavidVazquez001 <dvazquez.glife@gmail.com>
2024-03-07 20:12:55 -0600

    Se integra el metodo put en el posts.js

--- src/middlewares/userauth.js
+++ src/middlewares/userauth.js
@@ -1,29 +1,29 @@
 // const { findUserById } = require('../util/fileSystem')
-const Users = require('../models/model_users')
-const jwt = require('jsonwebtoken')
+const Users = require("../models/model_users");
+const jwt = require("jsonwebtoken");
 
 // MIDDLEWARE
 const validUser = async (req, res, next) => {
   // const { userid } = req.headers
   // const user = await Users.findById(userid)
-  const { authorization } = req.headers
-  const token = authorization.split(' ')[1] // se retiro la parte que dice Bearer en el header
+  const { authorization } = req.headers;
+  const token = authorization.split(" ")[1]; // se retiro la parte que dice Bearer en el header
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SIGN)
-    console.log(decoded)
-    const date = Math.floor(new Date().getTime() / 1000)
-    console.log(date)
+    const decoded = jwt.verify(token, process.env.JWT_SIGN);
+    console.log(decoded);
+    const date = Math.floor(new Date().getTime() / 1000);
+    console.log(date);
     if (decoded.exp < date) {
-      res.status(401).send({ message: 'session expired' })
+      res.status(401).send({ message: "session expired" });
     } else {
-      req.user = decoded
-      next()
+      req.user = decoded;
+      next();
     }
   } catch (error) {
-    res.status(401).send('Login is required')
+    res.status(401).send("Login is required");
   }
-}
+};
 
 module.exports = {
-  validUser
-}
+  validUser,
+};
--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,66 +1,82 @@
 //Imports
 
-const express = require('express')
-const router = express.Router()
-const Users = require('../models/model_users')
-const auth = require('../middlewares/userauth')
-const Posts = require('../models/model_posts')
-
+const express = require("express");
+const router = express.Router();
+const Users = require("../models/model_users");
+const auth = require("../middlewares/userauth");
+const Posts = require("../models/model_posts");
 
 //Get posts Angel
 
-router.get('/', async (req, res) => {
-
-  try{
-
+router.get("/", async (req, res) => {
+  try {
     const posts = await Posts.find();
     console.log(posts);
-    res.send({message: 'OK', data: posts});
-
-  } catch {
-
-  }
-
-})
-
+    res.send({ message: "OK", data: posts });
+  } catch {}
+});
 
 // Get Post por id Sadiel
-router.get('/:id', async (req, res) => {
-    try {
-        const idReq  = req.params.id
-        const post = await Posts.findById( idReq )
-        res.status(200).send({ message: post })
-    } catch (error) {
-        res.status(400).send({ message: error })
-    }
-    })
-
+router.get("/:id", async (req, res) => {
+  try {
+    const idReq = req.params.id;
+    const post = await Posts.findById(idReq);
+    res.status(200).send({ message: post });
+  } catch (error) {
+    res.status(400).send({ message: error });
+  }
+});
 
 // Get Post por id
 
 //Post Posts Angie
 
-router.post('/:userid', async (req, res) => {
+router.post("/:userid", async (req, res) => {
   try {
-    const { userid } = req.params
-    let post = req.body
-    post.user = userid
-    const newPost = await Posts.create(post)
-    res.status(201).send({ message: 'new post created', data: newPost })
+    const { userid } = req.params;
+    let post = req.body;
+    post.user = userid;
+    const newPost = await Posts.create(post);
+    res.status(201).send({ message: "new post created", data: newPost });
   } catch (error) {
-    console.log(error)
-    res.status(400).send({ message: error })
+    console.log(error);
+    res.status(400).send({ message: error });
   }
-})
+});
 
 //Put Posts David
+router.put("/:postid", auth.validUser, async (req, res) => {
+  try {
+    const { postid } = req.params;
+    const postData = req.body;
 
+    const userIdFromToken = req.user.id;
+    console.log("Usuario desde token:", userIdFromToken);
 
+    const existingPost = await Posts.findById(postid);
+    console.log("Detalles del Post Existente:", existingPost);
 
+    if (!existingPost) {
+      return res.status(404).send({ message: "Post no encontrado" });
+    }
 
+    const updatedPost = await Posts.findByIdAndUpdate(postid, postData, {
+      new: true,
+    });
 
+    console.log("Post actualizado correctamente:", updatedPost);
+
+    res
+      .status(200)
+      .send({ message: "Post actualizado correctamente", data: updatedPost });
+  } catch (error) {
+    console.error("Error al actualizar el post:", error.message);
+    res
+      .status(400)
+      .send({ message: "Error al actualizar el post", error: error.message });
+  }
+});
 
 // Delete Posts Sadiel
-
 
 module.exports = router;
