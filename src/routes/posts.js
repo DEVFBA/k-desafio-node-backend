95aabd3b58114c903d8ab9eb9765e86de06965d0
DEVFBA <angenrique.gutierrez@gmail.com>
2024-03-07 19:21:04 -0600

    First Endepoints Version

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,42 +1,39 @@
+//Imports
+
 const express = require('express')
 const router = express.Router()
 const Users = require('../models/model_users')
 const auth = require('../middlewares/userauth')
 const Posts = require('../models/model_posts')
 
-//Imports
-const express = require('express')
-const router = express.Router()
-const dbpost = require ()
-
-
-
-
-
 
 //Get posts Angel
+
+router.get('/', async (req, res) => {
+
+  try{
+
+    const posts = await Posts.find();
+    console.log(posts);
+    res.send({message: 'OK', data: posts});
+
+  } catch {
+
+  }
+
+})
+
 
 // Get Post por id Sadiel
 router.get('/:id', async (req, res) => {
     try {
         const idReq  = req.params.id
-        const post = await dbpost.findById( idReq )
+        const post = await Posts.findById( idReq )
         res.status(200).send({ message: post })
     } catch (error) {
         res.status(400).send({ message: error })
     }
     })
-
-
-
-
-
-
-
-
-
-
-
 
 
 // Get Post por id
@@ -58,17 +55,12 @@ router.post('/:userid', async (req, res) => {
 
 //Put Posts David
 
+
+
+
+
+
 // Delete Posts Sadiel
 
 
-
-
-
-
-
-
-
-
-
-module.exports = router
-module.exports = router
+module.exports = router;
